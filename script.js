468c533e3e8ee9f2861b8d55449b05cce170b405
azambrano18 <azambrano@mejoreferido.cl>
2026-02-27 14:03:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function toggleMusic() {
-    const music = document.getElementById("bgMusic");
-    if (music.paused) {
-        music.play();
-    } else {
-        music.pause();
-    }
-}
+const intro = document.getElementById("introScreen");
+const main = document.getElementById("mainContent");
+const music = document.getElementById("bgMusic");
+
+intro.addEventListener("click", () => {
+    music.play();
+
+    intro.style.opacity = "0";
+
+    setTimeout(() => {
+        intro.style.display = "none";
+        main.style.opacity = "1";
+    }, 1500);
+});
